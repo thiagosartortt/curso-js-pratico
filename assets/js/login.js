cc6ebd37e726a881030b58532664f2263943fbb6
thiago <thigosartortt@gmail.com>
2025-07-28 22:01:05 -0300

    paginas do projeto codehotel criadas

---
+++ assets/js/login.js
@@ -0,0 +1,13 @@
+function fazerLogin(event){
+    event.preventDefault();
+    const usuario = document.getElementById("username").value.trim();
+    const senha = document.getElementById("senha").value.trim();
+
+    if (usuario === "root" && senha === "admin"){
+        window.location.href = "dashboard.html";
+    }else{
+        alert("Credenciais inválidas. Tente novamente");
+    }
+}
+
+document.getElementById("loginForm").addEventListener("submit", fazerLogin);
