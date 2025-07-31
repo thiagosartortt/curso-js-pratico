4257d63d63583628db3998aab62ac38fff1bac9e
thiago <thigosartortt@gmail.com>
2025-07-30 21:58:51 -0300

    restruturacao do projeto

--- assets/js/login.js
+++ assets/js/login.js
@@ -4,7 +4,7 @@ function fazerLogin(event){
     const senha = document.getElementById("senha").value.trim();
 
     if (usuario === "root" && senha === "admin"){
-        window.location.href = "dashboard.html";
+        window.location.href = "admin/dashboard";
     }else{
         alert("Credenciais inválidas. Tente novamente");
     }
